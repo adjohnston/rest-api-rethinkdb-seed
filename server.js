dbb4d3e9f2b0908cc931568ea9c84f5cfbe02e9e
Adam Johnston <adam.johnston3679@gmail.com>
2015-12-31 11:22:32 +0000

    change port number since it conflicts with rethinkdb

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const server = restify.createServer({
   name: 'seed-api'
 });
 
+//  index
+//  ---------------------------------------------------------------------------
 server.get('/', (req, res, next) => {
   res.send('Hello world');
   next();
 });
 
+//  named routes
+//  ---------------------------------------------------------------------------
 const posts = require(`${__dirname}/routes/posts`)(server);
 
-server.listen(8080, () => {
+//  start server
+//  ---------------------------------------------------------------------------
+server.listen(8000, () => {
   console.log(`${server.name} listening at ${server.url}`);
 });
