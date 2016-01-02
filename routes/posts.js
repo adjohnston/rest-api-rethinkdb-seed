27e2e99bffa661bc9de91ff0d5382cfbf624bcd9
Adam Johnston <adam.johnston3679@gmail.com>
2016-01-02 13:00:22 +0000

    import Post and make a POST request for making a new post

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,21 @@
+const Post = require('../models/post.js');
+
 module.exports = (server) => {
   server.get('/posts', (req, res, next) => {
     res.send('Posts');
+    next();
+  });
+
+  server.post('/posts/create', (req, res, next) => {
+    const newPost = new Post({
+      name: req.params.name,
+      message: req.params.message
+    });
+
+    newPost.save().then((res) => {
+      console.log(res);
+    });
+
     next();
   });
 };
