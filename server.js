0489abf877b8f85428b36b8f1e2e7abfbe68ea27
Adam Johnston <adam.johnston3679@gmail.com>
2016-01-02 13:36:03 +0000

    tidy up comments

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 const restify = require('restify');
 const config = require(`${__dirname}/config.js`);
 
+//  create server
 const server = restify.createServer({
   name: config.restify.name
 });
 
+//  use restify's bodyParser
 server.use(restify.bodyParser({
   mapParams: true
 }));
 
-//  index
-//  ---------------------------------------------------------------------------
+//  index route
 server.get('/', (req, res, next) => {
   res.send('Hello world');
   next();
 });
 
 //  named routes
-//  ---------------------------------------------------------------------------
 const posts = require(`${__dirname}/routes/posts`)(server);
 
 //  start server
-//  ---------------------------------------------------------------------------
 server.listen(config.restify.port, () => {
   console.log(`${server.name} listening at ${server.url}`);
 });
